10532d569d6dd2e24d253700ba069c59e24cc5fd
Nolway <faizeau.alexis@gmail.com>
2023-09-11 11:46:24 +0200

    chore: release 1.0.0
    Release-As: 1.0.0

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+import plugin from "tailwindcss/plugin";
+import baseConfig from "./baseConfig";
+import base from "../dist/style/base";
+import utilities from "../dist/style/utilities";
+import components from "../dist/style/components";
+import themes from "./theming/themes";
+import colorFunctions from "./theming/colorFunctions";
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function main({ addBase, addComponents, config }) {
+    console.log("☕ WorkAdventure UI");
+    // Add base styles
+    addBase(base);
+    // Add utilities
+    addComponents(utilities, { variants: ["responsive"] });
+    // Add components
+    addComponents(components);
+    const themeInjectorHsl = colorFunctions.injectThemes(addBase, config, themes);
+    themeInjectorHsl;
+}
+export default plugin(main, baseConfig);
