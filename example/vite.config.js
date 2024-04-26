d2719f4c04aaa9e4c4c92dd21db08a1bc897b023
Gregoire Parant <gregoire@workadventu.re>
2024-04-26 15:15:34 +0200

    fix: Include bot html

--- example/vite.config.js
+++ example/vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     base: "./",
+    build: {
+        rollupOptions: {
+            input: {
+                index: "./index.html",
+                botList: "./bot-list.html",
+                botEdit: "./bot-edit.html",
+                botLogs: "./bot-logs.html",
+            },
+        },
+    },
     server: {
         open: true,
     },
