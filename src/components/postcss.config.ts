50aacba537d128892ac9ca7cd0431d93575e98c8
David Négrier <d.negrier@thecodingmachine.com>
2025-03-19 16:24:15 +0100

    fix: Inlining SVGs
    SVGs were not correctly inlined. This commit adds "postcss-inline-svg" that offers a way to inline SVGs into CSS automatically.

--- src/components/postcss.config.ts
+++ src/components/postcss.config.ts
@@ -3,6 +3,7 @@ module.exports = {
         require("postcss-import"),
         require("tailwindcss/nesting"),
         require("@csstools/postcss-sass"),
+        require("postcss-inline-svg"),
         // eslint-disable-next-line @typescript-eslint/no-var-requires
         require("tailwindcss")("./src/components/tailwind.config.ts"),
     ],
