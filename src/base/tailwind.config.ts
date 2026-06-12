f38a17ae27554e79abbd60edd694ba84d3955118
Ange Rollet <37471613+angerenage@users.noreply.github.com>
2026-06-12 10:17:18 +0200

    refactor!: Migration to tailwind 4 (#50)
    * refactor: migrate to tailwind 4
    
    * refactor: remove theming and watcher typescript files to use only css
    
    * refactor: migrate to Vite for build process and remove PostCSS configurations
    
    * refactor: get rid of eslint and typescript and bump dependencies
    
    * refactor: remove the build step for the design system
    
    * refactor: remove example package.json and update main package.json to integrate it
    
    * chore: remove main vite config and example typescript config

--- src/base/tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "tailwindcss";
-import baseConfig from "../baseConfig";
-
-export default {
-    content: [{ raw: "" }],
-    ...baseConfig,
-} satisfies Config;
--- src/theming/themeDefaults.ts
+++
@@ -1,4 +0,0 @@
-export default {
-    themeOrder: ["default"],
-    variables: {},
-};
